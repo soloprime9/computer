a34b449ac0b2a40154e0754e7fe775f0024a36b3
soloprime9 <primeindia5750@gmail.com>
2024-12-28 02:30:41 +0530

    Aapke changes ka description yahaan likhein

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,77 @@
+import Link from "next/link";
+import React from "react";
+import handleSubmit from '../app/signup/page'
+
+const Navbar = () => {
+  handleSubmit == true;
+
+
+
+  return (
+    <header className="bg-gray-900 text-white">
+      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+        {/* Left Section: Logo */}
+        <div className="flex items-center space-x-3">
+          
+          <span className="text-3xl font-bold text-white"><Link href="/">Timoes</Link></span>
+        </div>
+
+        {/* Center Section: Search Bar */}
+        <div className="flex-grow max-w-lg mx-4">
+          <div className="relative">
+            <button
+            
+              type="text"
+              placeholder="Search libraries..."
+              className="w-30 py-2 pl-10 pr-4 bg-gray-800 text-white rounded-md placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            > <a href="/search-user"> Search</a></button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M11 5a7 7 0 110 14 7 7 0 010-14zm10 10l-4-4"
+              />
+            </svg>
+          </div>
+        </div>
+
+        {/* Right Section: Navigation Links */}
+        <nav className="flex items-center space-x-6">
+          <a
+            href="#"
+            className="text-lg font-bold text-white hover:text-yellow-100"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="text-lg font-bold text-white hover:text-yellow-100"
+          >
+            Explore
+          </a>
+          <a
+            href="#"
+            className="text-lg font-bold text-white hover:text-yellow-100"
+          >
+            About
+          </a>
+        </nav>
+
+      </div>
+
+   
+
+    </header>
+
+    
+  );
+};
+
+export default Navbar;
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,113 @@
+'use client';
+
+import axios from 'axios';
+import { useState, useCallback, useEffect } from 'react';
+import Link from 'next/link';
+
+
+
+
+
+
+
+const SearchBar = () => {
+  const [query, setquery] = useState('');
+  const [result, setresult] = useState([]);
+  const [error, seterror] = useState([]);
+  
+
+  useEffect ( () => {
+    if (query.trim() === "") {
+      seterror(false);
+      setresult([]);
+      return;
+      
+    }
+
+    
+      axios.get("http://localhost:4000/search", {
+        params: { query },
+      })
+      .then((result) => {
+      setresult(result.data);
+      // console.log(result.data)
+    })
+
+    .catch( (error)=> {
+      console.log(error);
+      seterror(error);
+    })
+  }, [query]);
+
+  const handleSearch = (e)=>{
+    e.preventDefault();
+    setquery(query);
+}
+
+  
+
+  const CopyText = (text) => {
+    navigator.clipboard.writeText(text);
+    alert("Text Copied ");
+   
+
+  }
+
+  return (
+    <div className='m-5'>
+        <form  className='m-10'>
+    
+    <div className='justify-center items-center text-center flex'>
+    <input type="text"
+        value={query}
+        placeholder="Search User"
+        onChange={(e) => setquery(e.target.value)}
+        className="w-full h-50 py-2 pl-10 pr-4 bg-gray-800 text-white  text-2xl font-bold rounded-md placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      /> 
+<button type="submit" className="w-200 h-50 m-1 py-2 pl-5 pr-4 bg-gray-800 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-2xl font-bold" onClick={handleSearch}>Search</button>
+</div>
+</form>
+
+
+        {result && result.length > 0 ? (
+result.map((user) => (
+<div key={user._id} className="w-full mb-5 py-2 pl-10 pr-4 bg-gray-800 text-white rounded-md placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500">
+  <div className='flex justify-between'> 
+  <h1 className='text-3xl font-bold' ><Link href={"/detail/" + user._id} className='hover:underline'>{user.name}</Link></h1>
+
+  <input type="text" value={user.command} onClick={() => [CopyText(user.command), ] } readOnly className=' rounded text-blue-600 text-center text-2xl font-bold cursor-pointer p-2'/>
+  
+</div>
+  
+  <div className='flex space-x-10'>
+  {user.tags.map((tag, index) => (
+   
+        <div key={index} className='m-3'>
+          <button className='border-2 rounded p-1 text-lg'><FaCheck className='inline m-1'/>{tag}</button>
+          </div> 
+      ))}
+      </div>
+
+
+  <p className='text-lg my-3'>{user.description.substring(0, 40)}...</p>
+
+  <div className='flex space-x-10' >
+  <p > <a href={user.github_url} target='_blank'> <FaEarthAsia className="inline mr-2" /> Website</a></p>
+  <p > <a href={user.github_url}> <FaEarthAsia className="inline mr-2" /> Website</a></p>
+  <p > <a href={user.github_url}> <FaEarthAsia className="inline mr-2" /> Website</a></p>
+  <p > <a href={user.github_url}> <FaEarthAsia className="inline mr-2" /> Website</a></p>
+</div>
+</div>
+))
+) : (
+
+<div>...</div>
+
+)}
+
+    </div>
+)
+
+}
+
+export default SearchBar;
