6855a6e2d51c25affa22c8e2fa48c5618b9c97eb
soloprime9 <primeindia5750@gmail.com>
2024-12-30 16:15:59 +0530

    Error

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -104,7 +104,7 @@ const Profile = () => {
     <div className="max-w-5xl mx-auto p-4 md:p-6 lg:p-8">
       <div className="flex justify-between items-center mb-4">
         <div className="flex items-center">
-          <img src={user.profilePicture || "/1.jpg"} alt="Profile Picture" className="img-fluid rounded-circle w-12 h-12 rounded-full mr-4 " />
+          <img src={user.profilePicture || "/1.jpg"} alt="Profile Picture" className=" w-12 h-12 rounded-full mr-4 " />
           <h1 className="text-2xl font-bold">{user.username}</h1>
         </div>
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/Components/Footer";
 import Navbar from "@/Components/Navbar";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,24 +23,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      
-      <link 
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
-  rel="stylesheet" 
-/>
-
-
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link 
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
+          rel="stylesheet" 
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <Navbar />
+        <Navbar />
         {children}
         <Footer />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
