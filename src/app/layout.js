fde0c5efbbe2e96d7daec085829f68c9bda2c214
soloprime9 <primeindia5750@gmail.com>
2024-12-30 15:36:33 +0530

    bootstrap aur image problem fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      
+      <link 
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
+  rel="stylesheet" 
+/>
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
