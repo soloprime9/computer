61064e6d5bfe7076daf0953811492b819dfb291d
soloprime9 <primeindia5750@gmail.com>
2025-01-06 02:50:49 +0530

    Update SearchUser.jsx

--- src/Components/SearchUser.jsx
+++ src/Components/SearchUser.jsx
@@ -46,9 +46,9 @@ const SearchUser = () => {
         value={query}
         placeholder="Search User"
         onChange={(e) => setquery(e.target.value)}
-        className="w-full h-50 py-2 pl-10 pr-4 bg-gray-800 text-white  text-2xl font-bold rounded-md placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full h-50 py-2 pl-10 pr-4 bg-black text-white  text-2xl font-bold rounded-md placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 text-around"
       /> 
-<button type="submit" className="w-200 h-50 m-1 py-2 pl-5 pr-4 bg-gray-800 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-2xl font-bold" onClick={handleSearch}>Search</button>
+<button type="submit" className="w-200 h-50 m-1 py-2 pl-5 p-4 bg-blue-500 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-2xl font-bold" onClick={handleSearch}>Search</button>
 </div>
 </form>
 
