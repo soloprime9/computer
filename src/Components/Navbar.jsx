137959e06a4b652cefa812e920f053065705313c
soloprime9 <primeindia5750@gmail.com>
2025-02-24 17:45:51 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,8 +23,8 @@ const Navbar = () => {
             
               type="text"
               placeholder="Search libraries..."
-              className="w-30 py-2 pl-10 pr-4 bg-gray-800 text-white rounded-md placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            > <a href="/search-user" className="text-lg font-bold text-white hover:text-yellow-100"> Search</a></p>
+              className="text-3xl font-bold text-white"><Link href="/" className="text-lg font-bold text-white hover:text-yellow-100"
+            > <a href="/search-user" > Search</a></p>
             
           </div>
         </div>
