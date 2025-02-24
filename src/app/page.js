6203ddf1bbe44315059e4716a8629f8f522bd11b
soloprime9 <primeindia5750@gmail.com>
2025-02-24 15:24:51 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -103,7 +103,7 @@ const Feed = () => {
   return (
    <div className=" m-2 ">
       <div className="lg:flex md:justify-between border-2 p-2">
-        <div className="">
+        <div className="md:hidden">
           <p>Hello World</p>
 
         </div>
@@ -177,7 +177,7 @@ const Feed = () => {
         <p>No posts available</p>
       )}
       </div>
-      <div>
+      <div className="hidden">
         <p>Loream Heumdraid Bro</p>
       </div>
     </div>
