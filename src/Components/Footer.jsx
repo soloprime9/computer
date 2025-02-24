4bd82c38765efa096c2204935d16405f736d763b
soloprime9 <primeindia5750@gmail.com>
2025-02-24 16:21:16 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,25 +2,22 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 bg-gray-100 w-full">
-   <div className='flex justify-between m-4'>
-    <div>
-        <h1 className=' text-xl font-bold'>Join Us</h1>
+    <div className="fixed bottom-0 left-0 w-full bg-gray-100">
+      <div className='flex justify-between m-4'>
+        <div>
+          <h1 className=' text-xl font-bold'>Join Us</h1>
+        </div>
+        <div>
+          <h1 className=' text-xl font-bold'>About Us</h1>
+        </div>
+        <div>
+          <h1 className=' text-xl font-bold'>Contact Us</h1>
+        </div>
+      </div>
+      <div className='text-center mb-2'>
+        <p>This is created with Mern Language</p>
+      </div>
     </div>
-    <div>
-        <h1 className=' text-xl font-bold'>About Us</h1>
-    </div>
-    <div>
-        <h1 className=' text-xl font-bold'>Contact Us</h1>
-
-    </div>
-
-   </div>
-
-   <div className='text-center'>
-      <p>This is created with Mern Language</p>
-   </div>
-   </div>
   )
 }
 
