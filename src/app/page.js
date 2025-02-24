880c807defc3f61d2d21e80719140eed3b6c69e6
soloprime9 <primeindia5750@gmail.com>
2025-02-24 07:27:08 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -16,33 +16,33 @@ const Feed = () => {
 
 
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
+  // useEffect(() => {
+  //   const token = localStorage.getItem("token");
 
-    if (!token) {
-      console.log("Token is not available");
-      return (window.location.href = "/login");
-    }
+  //   if (!token) {
+  //     console.log("Token is not available");
+  //     return (window.location.href = "/login");
+  //   }
 
-    try {
-      const decoded = jwt.decode(token);
-      console.log("Decoded token data:", decoded);
-      if(!decoded || !decoded.exp){
-        console.log("Token or Exp Missing");
-        localStorage.removeItem("token");
-        window.location.href = "/login";
-      }  
-      if(decoded.exp * 1000 < Date.now()){
-        console.log("Now Going to Redirect on Login Page");
-        localStorage.removeItem("token");
-        window.location.href="/login";
-      }
-    } catch (err) {
-      console.log("Invalid Token:", err);
-      localStorage.removeItem("token");
-      return (window.location.href = "/login");
-    }
-  }, []);
+  //   try {
+  //     const decoded = jwt.decode(token);
+  //     console.log("Decoded token data:", decoded);
+  //     if(!decoded || !decoded.exp){
+  //       console.log("Token or Exp Missing");
+  //       localStorage.removeItem("token");
+  //       window.location.href = "/login";
+  //     }  
+  //     if(decoded.exp * 1000 < Date.now()){
+  //       console.log("Now Going to Redirect on Login Page");
+  //       localStorage.removeItem("token");
+  //       window.location.href="/login";
+  //     }
+  //   } catch (err) {
+  //     console.log("Invalid Token:", err);
+  //     localStorage.removeItem("token");
+  //     return (window.location.href = "/login");
+  //   }
+  // }, []);
 
   const fetchPosts = async () => {
     try {
