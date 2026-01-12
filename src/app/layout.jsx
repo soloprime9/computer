a545b803b43450c196abeb6d3c098c03e38dd898
soloprime9 <primeindia5750@gmail.com>
2026-01-12 17:10:47 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,22 +23,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link 
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
-          rel="stylesheet" 
-        />
+        
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
+      <body>
+        
         {children}
-        <Footer />
-        <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-          strategy="afterInteractive"
-        />
+        
+        
       </body>
     </html>
   );
