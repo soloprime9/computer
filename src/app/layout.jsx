984c686039b2d52baa29eda890bb6180bdb00084
soloprime9 <primeindia5750@gmail.com>
2026-01-14 16:16:47 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,31 +2,38 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/Components/Footer";
 import Navbar from "@/Components/Navbar";
-import Script from "next/script";
+
+export const metadata = {
+  manifest: "/site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" }
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180" }
+    ]
+  }
+};
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
-
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        
-      </head>
-      <body>
-        
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Navbar />
         {children}
-        
-        
+        <Footer />
       </body>
     </html>
   );
