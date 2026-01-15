27fa25b72f07bbf673190755e89b6181e4b724f6
soloprime9 <primeindia5750@gmail.com>
2026-01-16 01:11:06 +0530

    Create page.jsx

---
+++ src/app/[source]/[pid]/[...slug]/page.jsx
@@ -0,0 +1,17 @@
+;
+import SearchBar from '@/Components/SearchBar';
+import PostDetail from '@/Components/PostDetail';
+import React from 'react';
+
+
+
+const Single = () => {
+  return (
+    <div className='md:m-1'>
+        <PostDetail />
+        
+    </div>
+  )
+}
+
+export default Single;
